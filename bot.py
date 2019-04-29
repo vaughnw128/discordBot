dbbe6413af03e5dba5648644d1129e9f43f06203
Zetsu128 <vaughnwoerpel@gmail.com>
2019-04-28 23:25:45 -0400

    Added ability to add and remove things from the cringe list

--- bot.py
+++ bot.py
@@ -1,20 +1,85 @@
 # Work with Python 3.6
 import discord
 
-TOKEN = 'NTcxMTEzMTExMTMwNDcyNDQ4.XMYvKQ.ctbsjlaPAP8jmovfJIUcJBqK2c0'
+#TODO: allow for spaces in the terms, respond to cringe, display a PMed cringe list, voice chat features
 
 client = discord.Client()
 
+#list of terms
+terms = []
+
+def updateTerms(terms):
+    with open("terms.txt", "r") as file:    
+        rawTerms = file.readlines()
+        terms = [x.strip() for x in rawTerms]
+ 
+def addTerm(term):
+    terms.append(term)
+    with open("terms.txt", "a") as file:
+        if len(terms) > 0:
+            index = len(terms)-1
+        else:
+            index = 0
+        file.write(terms[index] + '\n')
+    updateTerms(terms)
+
+def delTerm(term):
+    terms.remove(term)
+    with open("terms.txt", "r") as file:
+        lines = file.readlines()
+    with open("terms.txt", "w") as file:
+        for line in lines:
+            if line.strip("\n") != term:
+                file.write(line)
+    updateTerms(terms)
+
 @client.event
 async def on_message(message):
-    # we do not want the bot to reply to itself
-
     if message.author == client.user:
         return
 
     if message.content.startswith('c!status'):
         msg = 'Cringedar: Active'.format(message)
         await message.channel.send(msg)
+
+    #Adding a term
+    if message.author.id == 173232081575346178:
+        if message.content.startswith('c!addterm'):
+            term = (message.content.split())
+            
+            if len(term) < 2:
+                msg = 'Please include a term.'
+            else:
+                tempTerm = ""
+                for x in range(1, len(term)):
+                    tempTerm += term[x]
+                    if(x != len(term)-1):
+                        tempTerm += ' '
+                term = tempTerm
+                if term in terms:
+                    msg = 'Term already exists.'
+                else:
+                    addTerm(term)
+                    msg = 'Added ' + term + ' to cringe list.'.format(message)
+            await message.channel.send(msg)
+        if message.content.startswith('c!delterm'):
+            term = (message.content.split())
+            if len(term) < 2:
+                msg = 'Please include a term.'
+            else:
+                term = term[1]
+                delTerm(term)
+                msg = 'Removed ' + term + ' from the cringe list.'.format(message)
+            await message.channel.send(msg)
+        if message.content.startswith('c!cringelist'):
+            msg = '``` \n'
+            for x in terms:
+                msg += x + '\n'
+            msg += '```'
+            msg.format(message)
+            await message.channel.send(msg)
+
+
 
 @client.event
 async def on_ready():
@@ -23,4 +88,7 @@ async def on_ready():
     print(client.user.id)
     print('------')
 
+#Starts bot
+tfile = open("token.txt", "r")
+TOKEN = tfile.read()
 client.run(TOKEN)
